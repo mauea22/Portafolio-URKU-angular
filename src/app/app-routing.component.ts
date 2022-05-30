f32a74d2fd800e132aa1ede1102829a882b2bed5
mauea22 <mauea22@hotmail.com>
2022-05-30 16:40:53 -0300

    about.component / item.component

---
+++ src/app/app-routing.component.ts
@@ -0,0 +1,24 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const app_route: Routes = [
+    {path:'', component: PortafolioComponent},
+    {path:'about', component:AboutComponent },
+    {path:'item', component: ItemComponent },
+    {path:'**', pathMatch: 'full', redirectTo: '' }
+];
+
+
+
+@NgModule({
+    imports:[
+        RouterModule.forRoot(app_route)
+    ],
+    exports:[
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
