71abe8c6a9ab427513060cde5a4d84d930094424
mauea22 <mauea22@hotmail.com>
2022-05-31 12:08:11 -0300

    reemplazar info de la pagina usando servicio

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  info: any = {};
+  info: InfoPagina = {}; //info pagina es la referencia a la interface de los datos almacenados
   cargada = false;
 
 
   constructor( private http: HttpClient) { 
     //leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp:any )=> {
+      .subscribe( (resp: InfoPagina )=> {
         
         this.cargada = true;
         this.info = resp;
